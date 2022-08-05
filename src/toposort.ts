e10e17abcffbf7ba4af00625815ff7805b61b83d
Otavio Macedo <otaviomacedo@protonmail.com>
2022-08-05 20:18:52 +0100

    Improved comments

--- src/toposort.ts
+++ src/toposort.ts
@@ -11,19 +11,21 @@ export function topologicalSort(resources: any): [string, any][] {
   Object.keys(resources).forEach(visit);
   return sorted.map((id: string) => [id, resources[id]]);
 
-  // recursive function to visit the graph formed by the resources,
-  // in a depth-first way.
-  // If resource A has a reference to resource B, then A will be visited
-  // before B.
+  /**
+   * Recursive function to visit, in a depth-first way, the graph
+   * formed by the resources, starting from a given node.
+   *
+   * If resource A has a transitive reference to resource B, then A
+   * will be visited after B. When a resource is visited for the
+   * first time, it is appended to the result. Therefore, the id of
+   * A will appear after the id of B in the final result.
+   *
+   * @param id the id of the starting point of the visit
+   */
   function visit(id: string) {
-    // Since we're going depth-first, the stack corresponds to a path in the graph;
-    // So if any node appears again, it is a back edge, which creates a cycle.
-    if (stack.includes(id)) {
-      const path = stack.concat(id).slice(stack.indexOf(id)).join(' -> ');
-      throw new Error(`Cycle detected: ${path}`);
-    }
+    assertNoCycle(id);
 
-    // No cycles. Proceed with the DFS
+    // No cycle detected. Proceed with the DFS
     if (!hasBeenVisited(id)) {
       stack.push(id);
       adjacent(id).forEach(visit);
@@ -32,10 +34,25 @@ export function topologicalSort(resources: any): [string, any][] {
     }
   }
 
+  function assertNoCycle(id: string) {
+    // Since we're going depth-first, the stack corresponds to a path in the graph;
+    // So if any node appears again, we have a back edge, which creates a cycle.
+    if (stack.includes(id)) {
+      const path = stack.concat(id).slice(stack.indexOf(id)).join(' -> ');
+      throw new Error(`Cycle detected: ${path}`);
+    }
+  }
+
   function hasBeenVisited(id: string): boolean {
     return sorted.includes(id);
   }
 
+  /**
+   * Returns the list of ids referenced from anywhere in the resource
+   * definition.
+   *
+   * @param resourceId the logical id of the resource being queried.
+   */
   function adjacent(resourceId: string): string[] {
     const resource = resources[resourceId];
     const refs: Set<string> = new Set();
