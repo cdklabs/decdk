f7e706fd644c5cf67e411a705a8e09e2a2d091bc
Momo Kornher <kornherm@amazon.co.uk>
2022-08-23 11:45:11 +0100

    test: ignore non template-files in synth tests

--- test/synth.test.ts
+++ test/synth.test.ts
@@ -5,9 +5,17 @@ import { Testing } from './util';
 
 const dir = path.join(__dirname, '..', 'examples');
 
-for (const templateFile of fs.readdirSync(dir)) {
-  test(templateFile, async () => {
-    const template = await readTemplate(path.resolve(dir, templateFile));
+const isTemplateFile = (dirent: fs.Dirent): boolean =>
+  dirent.isFile() &&
+  (dirent.name.endsWith('.json') || dirent.name.endsWith('.yaml'));
+
+const examples = fs
+  .readdirSync(dir, { withFileTypes: true })
+  .filter(isTemplateFile);
+
+for (const templateFile of examples) {
+  test(templateFile.name, async () => {
+    const template = await readTemplate(path.resolve(dir, templateFile.name));
 
     const output = await Testing.synth(template);
     expect(output.template).toMatchSnapshot();
