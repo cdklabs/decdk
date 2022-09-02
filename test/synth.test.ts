b5e1a1508ab542f77b5296c53e345c4f8df3bc25
Momo Kornher <kornherm@amazon.co.uk>
2022-09-02 15:20:48 +0100

    chore: generic examples test case

--- test/synth.test.ts
+++ test/synth.test.ts
@@ -1,23 +1,9 @@
-import * as fs from 'fs';
-import * as path from 'path';
 import { readTemplate } from '../src';
-import { Testing } from './util';
+import { testExamples, Testing } from './util';
 
-const dir = path.join(__dirname, '..', 'examples');
+testExamples(async (templateFile) => {
+  const template = await readTemplate(templateFile.path);
 
-const isTemplateFile = (dirent: fs.Dirent): boolean =>
-  dirent.isFile() &&
-  (dirent.name.endsWith('.json') || dirent.name.endsWith('.yaml'));
-
-const examples = fs
-  .readdirSync(dir, { withFileTypes: true })
-  .filter(isTemplateFile);
-
-for (const templateFile of examples) {
-  test(templateFile.name, async () => {
-    const template = await readTemplate(path.resolve(dir, templateFile.name));
-
-    const output = await Testing.synth(template);
-    expect(output.template).toMatchSnapshot();
-  });
-}
+  const output = await Testing.synth(template);
+  expect(output.template).toMatchSnapshot();
+});
--- test/type-resolution/examples.test.ts
+++ test/type-resolution/examples.test.ts
@@ -1,33 +1,19 @@
-import * as fs from 'fs';
-import * as path from 'path';
 import * as reflect from 'jsii-reflect';
 import { readTemplate } from '../../src';
 import { resolveResourceLike } from '../../src/type-resolution';
-import { Testing } from '../util';
+import { testExamples, Testing } from '../util';
 
 let typeSystem: reflect.TypeSystem;
 beforeAll(async () => {
   typeSystem = await Testing.typeSystem;
 });
 
-const dir = path.join(__dirname, '..', '..', 'examples');
+testExamples(async (example) => {
+  const template = await readTemplate(example.path);
 
-const isTemplateFile = (dirent: fs.Dirent): boolean =>
-  dirent.isFile() &&
-  (dirent.name.endsWith('.json') || dirent.name.endsWith('.yaml'));
+  const typedTemplate = template
+    .resourceGraph()
+    .map((_, resource) => resolveResourceLike(resource, typeSystem));
 
-const examples = fs
-  .readdirSync(dir, { withFileTypes: true })
-  .filter(isTemplateFile);
-
-for (const templateFile of examples) {
-  test(templateFile.name, async () => {
-    const template = await readTemplate(path.resolve(dir, templateFile.name));
-
-    const typedTemplate = template
-      .resourceGraph()
-      .map((_, resource) => resolveResourceLike(resource, typeSystem));
-
-    expect(typedTemplate).toMatchSnapshot();
-  });
-}
+  expect(typedTemplate).toMatchSnapshot();
+});
