375d1e582b8fa61a39ced6f8d678333193827373
Momo Kornher <kornherm@amazon.co.uk>
2022-09-07 11:24:29 +0100

    chore: cleanup

--- src/parser/private/util.ts
+++
@@ -1,9 +0,0 @@
-export function mkDict<A>(
-  xs: ReadonlyArray<readonly [string, A]>
-): Record<string, A> {
-  const ret: Record<string, A> = {};
-  for (const [k, v] of xs) {
-    ret[k] = v;
-  }
-  return ret;
-}
