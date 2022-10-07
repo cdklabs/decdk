28f5b2a29752390687d352f645c99c857b7fde73
Michael Luk <michaluk@amazon.com>
2022-10-07 15:55:48 -0700

    feat: specs generation for ConstructProps

--- src/decdk-specs.ts
+++ src/decdk-specs.ts
@@ -1,7 +1,12 @@
+import { generateDeCDKSpecs } from './specs/specs-generation';
+import { loadTypeSystem } from './util';
+
 /* eslint-disable no-console */
 
 async function main() {
-  console.log('{}');
+  const typeSystem = await loadTypeSystem();
+  const specs = await generateDeCDKSpecs(typeSystem);
+  console.log(JSON.stringify(specs, undefined, 2));
 }
 
 main().catch((e) => {
---
+++ src/specs/specs-generation.ts
@@ -0,0 +1,81 @@
+import * as reflect from 'jsii-reflect';
+import { Property } from 'jsii-reflect';
+import { hasPropsParam } from '../type-system';
+
+export function generateDeCDKSpecs(typeSystem: reflect.TypeSystem) {
+  const moduleName = 'aws_lambda';
+  const constructs = getConstructs(typeSystem, moduleName);
+  const resourcesSpecs = buildSpecsForResources(constructs, typeSystem);
+  return {
+    ModuleTypes: {
+      [`aws-cdk-lib.${moduleName}`]: {
+        ResourceTypes: resourcesSpecs,
+      },
+    },
+  };
+}
+
+function getConstructs(typeSystem: reflect.TypeSystem, namespace: string) {
+  const constructType = typeSystem.findClass('constructs.Construct');
+  const cfnResourceType = typeSystem.findClass('aws-cdk-lib.CfnResource');
+
+  return typeSystem.classes.filter(
+    (c) =>
+      c.extends(constructType) &&
+      !c.extends(cfnResourceType) &&
+      c.spec.namespace === namespace
+  );
+}
+
+function buildSpecsForResources(
+  constructs: reflect.ClassType[],
+  typeSystem: reflect.TypeSystem
+) {
+  const specsList = constructs.map((c: reflect.ClassType) => {
+    return {
+      [c.name]: {
+        Properties: buildSpecsForResourceProperties(c, typeSystem),
+      },
+    };
+  });
+  return mergeToObject(specsList);
+}
+
+function buildSpecsForResourceProperties(
+  c: reflect.ClassType,
+  typeSystem: reflect.TypeSystem
+) {
+  if (!hasPropsParam(c, 2)) return;
+
+  const propFqn = c.initializer?.parameters?.[2]?.type.fqn;
+  var resourceProperties = {};
+  if (propFqn != undefined) {
+    const propInterface = typeSystem.findInterface(propFqn);
+
+    resourceProperties = mergeToObject(
+      propInterface.allProperties.map((p: Property) => ({
+        [p.name]: {
+          Remarks: p.docs.remarks,
+          Summary: p.docs.summary,
+          Required: p.optional == null || p.optional == true ? 'False' : 'True',
+          Type: formatSpecsType(p.type),
+        },
+      }))
+    );
+  }
+  return resourceProperties;
+}
+
+function formatSpecsType(type: reflect.TypeReference) {
+  if (type.fqn != null) {
+    return type.fqn;
+  } else if (type.primitive != null) {
+    return type.primitive.charAt(0).toUpperCase() + type.primitive.slice(1);
+  } else {
+    return `Unhandled Type: ${Object.keys(type)}`;
+  }
+}
+
+function mergeToObject(list: any[]) {
+  return Object.assign({}, ...list);
+}
