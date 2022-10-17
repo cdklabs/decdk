42bb99224e015dfb1d69293959ba0631fab3f912
Otavio Macedo <288203+otaviomacedo@users.noreply.github.com>
2022-10-17 11:49:23 +0100

    fix: transforms with parameters are not accepted (#379)
    Fixes https://github.com/cdklabs/decdk/issues/370.

--- test/evaluate/fixtures.test.ts
+++ test/evaluate/fixtures.test.ts
@@ -18,7 +18,6 @@ describe('Cloudformation templates', () => {
   const ignoreBecauseCurrentlyFailing: string[] = [
     'cloudformation/functions-and-conditions.json', // Expected list of length 3, got 2
     'cloudformation/hook-code-deploy-blue-green-ecs.json', // Expected valid template, got error(s): -  is not allowed to have the additional property "Hooks"
-    'cloudformation/parameter-references.json', // Expected string or list of strings, got: {"Name":"AWS::Include","Parameters":{"Location":{"Ref":"MyParam"}}}
     'cloudformation/resource-attribute-creation-policy.json', // Expected number, got: {"Ref":"CountParameter"}
     'cloudformation/resource-attribute-update-policy.json', // Expected boolean, got: {"Fn::Equals":["true",{"Ref":"WaitOnResourceSignals"}]}
   ];
