8546a7fb6248f3b4c37812bd93baa0595d7a3a43
Momo Kornher <kornherm@amazon.co.uk>
2022-10-20 13:10:00 +0100

    feat: add decdk metadata

--- test/evaluate/cdk-metadata.test.ts
+++ test/evaluate/cdk-metadata.test.ts
@@ -1,3 +1,4 @@
+import * as zlib from 'zlib';
 import { Capture } from 'aws-cdk-lib/assertions';
 import { Template } from '../../src/parser/template';
 import { Testing } from '../util';
@@ -42,11 +43,23 @@ test('has deCDK Metadata', async () => {
     }
   );
 
-  const analytics = new Capture();
+  const analyticsData = new Capture();
   template.hasResourceProperties('AWS::CDK::Metadata', {
-    Analytics: analytics,
+    Analytics: analyticsData,
   });
 
+  const analytics = extractAnalytics(analyticsData.asString());
+
   // THEN
-  expect(analytics.asString()).toContain('v2:deflate64');
+  expect(analytics).toContain('0.0.0!@cdklabs/decdk');
 });
+
+function extractAnalytics(analytics: string): any {
+  const [version, deflate, data] = analytics.split(':');
+
+  if (version !== 'v2' || deflate !== 'deflate64' || !data) {
+    throw new Error('Unexpected Analytics Data');
+  }
+
+  return zlib.gunzipSync(Buffer.from(data, 'base64')).toString();
+}
