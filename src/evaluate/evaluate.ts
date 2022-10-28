aaba0d45a8f1e84a3bc837f6151db3dacd09fe4a
Momo Kornher <kornherm@amazon.co.uk>
2022-10-28 12:17:56 +0100

    abstract evaluator

--- src/evaluate/evaluate.ts
+++ src/evaluate/evaluate.ts
@@ -55,8 +55,96 @@ import {
   ValueOnlyReference,
 } from './references';
 
-export class Evaluator {
+export abstract class BaseEvaluator {
   constructor(public readonly context: EvaluationContext) {}
+
+  public abstract evaluateMappings(ctx: AnnotationsContext): any;
+  public abstract evaluateParameters(ctx: AnnotationsContext): any;
+  public abstract evaluateResources(ctx: AnnotationsContext): any;
+  public abstract evaluateOutputs(ctx: AnnotationsContext): any;
+  public abstract evaluateTransform(ctx: AnnotationsContext): any;
+  public abstract evaluateMetadata(ctx: AnnotationsContext): any;
+  public abstract evaluateRules(ctx: AnnotationsContext): any;
+  public abstract evaluateHooks(ctx: AnnotationsContext): any;
+  public abstract evaluateConditions(ctx: AnnotationsContext): any;
+
+  protected abstract evaluateNull(): any;
+  protected abstract evaluateVoid(): any;
+  protected abstract evaluateDate(x: DateLiteral): any;
+  protected abstract evaluatePrimitive(
+    x: StringLiteral | NumberLiteral | BooleanLiteral
+  ): any;
+  protected abstract evaluateCfnResource(
+    x: CfnResourceNode,
+    ctx: AnnotationsContext
+  ): any;
+  protected abstract evaluateConstruct(
+    x: CdkConstruct,
+    ctx: AnnotationsContext
+  ): any;
+  protected abstract evaluateCdkObject(
+    x: CdkObject,
+    ctx: AnnotationsContext
+  ): any;
+  protected abstract evaluateObject(
+    xs: Record<string, TypedTemplateExpression>,
+    ctx: AnnotationsContext
+  ): Record<string, unknown>;
+  protected abstract evaluateArray(
+    xs: TypedTemplateExpression[],
+    ctx: AnnotationsContext
+  ): any;
+
+  protected abstract evaluateCall(
+    call: StaticMethodCallExpression | InstanceMethodCallExpression,
+    ctx: AnnotationsContext
+  ): any;
+  protected abstract evaluateEnum(fqn: string, choice: string): any;
+  protected abstract evaluateInitializer(
+    fqn: string,
+    parameters: unknown[]
+  ): any;
+
+  protected abstract fnBase64(x: string): any;
+  protected abstract fnCidr(
+    ipBlock: string,
+    count: number,
+    sizeMask?: string
+  ): any;
+  protected abstract fnFindInMap(
+    mapName: string,
+    topLevelKey: string,
+    secondLevelKey: string
+  ): any;
+  protected abstract fnGetProp(logicalId: string, prop: string): any;
+  protected abstract fnGetAtt(logicalId: string, attribute: string): any;
+  protected abstract fnGetAzs(region: string): any;
+  protected abstract fnIf(
+    conditionName: string,
+    ifYes: TypedTemplateExpression,
+    ifNo: TypedTemplateExpression,
+    ctx: AnnotationsContext
+  ): any;
+  protected abstract fnImportValue(exportName: string): any;
+  protected abstract fnJoin(separator: string, array: any[]): any;
+  protected abstract fnRef(logicalId: string): any;
+  protected abstract fnSelect(index: number, elements: any[]): any;
+  protected abstract fnSplit(separator: string, value: string): any;
+  protected abstract fnSub(
+    fragments: SubFragment[],
+    additionalContext: Record<string, any>
+  ): any;
+  protected abstract fnTransform(
+    transformName: string,
+    parameters: Record<string, unknown>
+  ): any;
+  protected abstract fnAnd(operands: unknown[]): any;
+  protected abstract fnOr(operands: unknown[]): any;
+  protected abstract fnNot(operand: unknown): any;
+  protected abstract fnEquals(value1: unknown, value2: unknown): any;
+  protected abstract fnLazyLogicalId(x: LazyLogicalId): any;
+  protected abstract fnLength(x: unknown): any;
+  protected abstract fnToJsonString(x: unknown): any;
 
   public evaluateTemplate(ctx: AnnotationsContext) {
     this.evaluateParameters(ctx.child('Parameters'));
@@ -70,6 +158,154 @@ export class Evaluator {
     this.evaluateHooks(ctx.child('Hooks'));
   }
 
+  public evaluate(x: TypedTemplateExpression, ctx: AnnotationsContext): any {
+    try {
+      switch (x.type) {
+        case 'null':
+          return this.evaluateNull();
+        case 'void':
+          return this.evaluateVoid();
+        case 'string':
+        case 'number':
+        case 'boolean':
+          return this.evaluatePrimitive(x);
+        case 'date':
+          return this.evaluateDate(x);
+        case 'array':
+          return this.evaluateArray(x.array, ctx);
+        case 'struct':
+        case 'object':
+          return this.evaluateObject(x.fields, ctx);
+        case 'resolve-reference':
+          return this.resolveReference(x.reference, ctx);
+        case 'intrinsic':
+          return this.evaluateIntrinsic(x, ctx);
+        case 'enum':
+          return this.evaluateEnum(x.fqn, x.choice);
+        case 'staticProperty':
+          return this.evaluateEnum(x.fqn, x.property);
+        case 'any':
+          return this.evaluate(x.value, ctx);
+        case 'lazyResource':
+          return this.evaluateCall(x.call, ctx.child('Call'));
+        case 'construct':
+          return this.evaluateConstruct(x, ctx);
+        case 'cdkObject':
+          return this.evaluateCdkObject(x, ctx);
+        case 'resource':
+          return this.evaluateCfnResource(x, ctx);
+        case 'initializer':
+          return ctx
+            .child(x.fqn)
+            .wrap((innerCtx) =>
+              this.evaluateInitializer(
+                x.fqn,
+                this.evaluateArray(x.args.array, innerCtx)
+              )
+            );
+        case 'staticMethodCall':
+          return this.evaluateCall(x, ctx);
+      }
+    } catch (error) {
+      ctx.error(RuntimeError.wrap(error));
+    }
+  }
+
+  protected evaluateIntrinsic(
+    x: IntrinsicExpression,
+    ctx: AnnotationsContext
+  ): any {
+    if (x.fn === 'lazyLogicalId') {
+      return this.fnLazyLogicalId(x);
+    }
+
+    return ctx.child(intrinsicToLongForm(x.fn)).wrap((intrinsicCtx) => {
+      const ev = (y: TypedTemplateExpression) => this.evaluate(y, intrinsicCtx);
+      const maybeEv = (y?: TypedTemplateExpression): any =>
+        y ? ev(y) : undefined;
+
+      switch (x.fn) {
+        case 'base64':
+          return this.fnBase64(assertString(ev(x.expression)));
+        case 'cidr':
+          return this.fnCidr(ev(x.ipBlock), ev(x.count), maybeEv(x.netMask));
+        case 'findInMap':
+          return this.fnFindInMap(
+            assertString(ev(x.mappingName)),
+            assertString(ev(x.key1)),
+            assertString(ev(x.key2))
+          );
+        case 'getAtt':
+          return this.fnGetAtt(x.logicalId, assertString(ev(x.attribute)));
+        case 'getProp':
+          return this.fnGetProp(x.logicalId, assertString(x.property));
+        case 'getAzs':
+          return this.fnGetAzs(assertString(ev(x.region)));
+        case 'if':
+          return this.fnIf(x.conditionName, x.then, x.else, intrinsicCtx);
+        case 'importValue':
+          return this.fnImportValue(assertString(ev(x.export)));
+        case 'join':
+          return this.fnJoin(assertString(x.separator), ev(x.list));
+        case 'ref':
+          return this.fnRef(x.logicalId);
+        case 'select':
+          return this.fnSelect(ev(x.index), ev(x.objects));
+        case 'split':
+          return this.fnSplit(x.separator, assertString(ev(x.value)));
+        case 'sub':
+          return this.fnSub(
+            x.fragments,
+            this.evaluateObject(x.additionalContext, intrinsicCtx)
+          );
+        case 'transform':
+          return this.fnTransform(
+            x.transformName,
+            this.evaluateObject(x.parameters, intrinsicCtx)
+          );
+        case 'and':
+          return this.fnAnd(x.operands.map((o) => ev(o)));
+        case 'or':
+          return this.fnOr(x.operands.map((o) => ev(o)));
+        case 'not':
+          return this.fnNot(ev(x.operand));
+        case 'equals':
+          return this.fnEquals(ev(x.value1), ev(x.value2));
+        case 'args':
+          return this.evaluateArray(x.array, intrinsicCtx);
+        case 'length':
+          return this.fnLength(ev(x.list));
+        case 'toJsonString':
+          return this.fnToJsonString(
+            this.evaluateObject(x.value, intrinsicCtx)
+          );
+      }
+    });
+  }
+
+  protected resolveReference(
+    intrinsic: RefIntrinsic | GetPropIntrinsic,
+    ctx: AnnotationsContext
+  ) {
+    const { logicalId, fn } = intrinsic;
+
+    if (fn !== 'ref') {
+      return this.evaluateIntrinsic(intrinsic, ctx);
+    }
+
+    return ctx.child('Ref').wrap(() => {
+      const c = this.context.reference(logicalId);
+
+      if (!c.instance) {
+        return this.fnRef(logicalId);
+      }
+
+      return c.instance;
+    });
+  }
+}
+
+export class Evaluator extends BaseEvaluator {
   public evaluateMappings(ctx: AnnotationsContext) {
     const scope = new Construct(this.context.stack, '$Mappings');
     this.context.template.mappings.forEach((mapping, mapName) =>
@@ -196,59 +432,6 @@ export class Evaluator {
     });
   }
 
-  public evaluate(x: TypedTemplateExpression, ctx: AnnotationsContext): any {
-    try {
-      switch (x.type) {
-        case 'null':
-          return this.evaluateNull();
-        case 'void':
-          return this.evaluateVoid();
-        case 'string':
-        case 'number':
-        case 'boolean':
-          return this.evaluatePrimitive(x);
-        case 'date':
-          return this.evaluateDate(x);
-        case 'array':
-          return this.evaluateArray(x.array, ctx);
-        case 'struct':
-        case 'object':
-          return this.evaluateObject(x.fields, ctx);
-        case 'resolve-reference':
-          return this._resolveReference(x.reference, ctx);
-        case 'intrinsic':
-          return this.evaluateIntrinsic(x, ctx);
-        case 'enum':
-          return this.evaluateEnum(x.fqn, x.choice);
-        case 'staticProperty':
-          return this.evaluateEnum(x.fqn, x.property);
-        case 'any':
-          return this.evaluate(x.value, ctx);
-        case 'lazyResource':
-          return this.evaluateCall(x.call, ctx.child('Call'));
-        case 'construct':
-          return this.evaluateConstruct(x, ctx);
-        case 'cdkObject':
-          return this.evaluateCdkObject(x, ctx);
-        case 'resource':
-          return this.evaluateCfnResource(x, ctx);
-        case 'initializer':
-          return ctx
-            .child(x.fqn)
-            .wrap((innerCtx) =>
-              this.evaluateInitializer(
-                x.fqn,
-                this.evaluateArray(x.args.array, innerCtx)
-              )
-            );
-        case 'staticMethodCall':
-          return this.evaluateCall(x, ctx);
-      }
-    } catch (error) {
-      ctx.error(RuntimeError.wrap(error));
-    }
-  }
-
   protected evaluateNull() {
     return undefined;
   }
@@ -318,75 +501,6 @@ export class Evaluator {
     return xs.map((x, idx) => this.evaluate(x, ctx.child(idx)));
   }
 
-  protected evaluateIntrinsic(x: IntrinsicExpression, ctx: AnnotationsContext) {
-    if (x.fn === 'lazyLogicalId') {
-      return this.fnLazyLogicalId(x);
-    }
-
-    return ctx.child(intrinsicToLongForm(x.fn)).wrap((intrinsicCtx) => {
-      const ev = (y: TypedTemplateExpression) => this.evaluate(y, intrinsicCtx);
-      const maybeEv = (y?: TypedTemplateExpression): any =>
-        y ? ev(y) : undefined;
-
-      switch (x.fn) {
-        case 'base64':
-          return this.fnBase64(assertString(ev(x.expression)));
-        case 'cidr':
-          return this.fnCidr(ev(x.ipBlock), ev(x.count), maybeEv(x.netMask));
-        case 'findInMap':
-          return this.fnFindInMap(
-            assertString(ev(x.mappingName)),
-            assertString(ev(x.key1)),
-            assertString(ev(x.key2))
-          );
-        case 'getAtt':
-          return this.fnGetAtt(x.logicalId, assertString(ev(x.attribute)));
-        case 'getProp':
-          return this.fnGetProp(x.logicalId, assertString(x.property));
-        case 'getAzs':
-          return this.fnGetAzs(assertString(ev(x.region)));
-        case 'if':
-          return this.fnIf(x.conditionName, x.then, x.else, intrinsicCtx);
-        case 'importValue':
-          return this.fnImportValue(assertString(ev(x.export)));
-        case 'join':
-          return this.fnJoin(assertString(x.separator), ev(x.list));
-        case 'ref':
-          return this.fnRef(x.logicalId);
-        case 'select':
-          return this.fnSelect(ev(x.index), ev(x.objects));
-        case 'split':
-          return this.fnSplit(x.separator, assertString(ev(x.value)));
-        case 'sub':
-          return this.fnSub(
-            x.fragments,
-            this.evaluateObject(x.additionalContext, intrinsicCtx)
-          );
-        case 'transform':
-          return this.fnTransform(
-            x.transformName,
-            this.evaluateObject(x.parameters, intrinsicCtx)
-          );
-        case 'and':
-          return this.fnAnd(x.operands.map((o) => ev(o)));
-        case 'or':
-          return this.fnOr(x.operands.map((o) => ev(o)));
-        case 'not':
-          return this.fnNot(ev(x.operand));
-        case 'equals':
-          return this.fnEquals(ev(x.value1), ev(x.value2));
-        case 'args':
-          return this.evaluateArray(x.array, intrinsicCtx);
-        case 'length':
-          return this.fnLength(ev(x.list));
-        case 'toJsonString':
-          return this.fnToJsonString(
-            this.evaluateObject(x.value, intrinsicCtx)
-          );
-      }
-    });
-  }
-
   protected evaluateCall(
     call: StaticMethodCallExpression | InstanceMethodCallExpression,
     ctx: AnnotationsContext
@@ -429,7 +543,7 @@ export class Evaluator {
     parameters: any[],
     ctx: AnnotationsContext
   ) {
-    const instance = this._resolveReference(target.reference, ctx);
+    const instance = this.resolveReference(target.reference, ctx);
     return instance[method](...parameters);
   }
 
@@ -440,27 +554,6 @@ export class Evaluator {
   ): any {
     const typeClass = this.context.resolveClass(fqn);
     return typeClass[method](...parameters);
-  }
-
-  private _resolveReference(
-    intrinsic: RefIntrinsic | GetPropIntrinsic,
-    ctx: AnnotationsContext
-  ) {
-    const { logicalId, fn } = intrinsic;
-
-    if (fn !== 'ref') {
-      return this.evaluateIntrinsic(intrinsic, ctx);
-    }
-
-    return ctx.child('Ref').wrap(() => {
-      const c = this.context.reference(logicalId);
-
-      if (!c.instance) {
-        return this.fnRef(logicalId);
-      }
-
-      return c.instance;
-    });
   }
 
   protected evaluateInitializer(fqn: string, parameters: unknown[]): any {
